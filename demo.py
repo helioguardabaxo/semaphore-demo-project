8559641fe81048c91abad2122388aa72da7362d3
helioguardabaxo <helio.guardabaxo@gmail.com>
2024-08-26 17:11:31 -0300

    json test

--- demo.py
+++ demo.py
@@ -1,5 +1,11 @@
+# import sys
+
+# name = str(sys.argv[1])
+
+# print('Hello, {0}! Welcome to Semaphore with Python!'.format(name))
+
+import json
 import sys
 
-name = str(sys.argv[1])
-
-print('Hello, {0}! Welcome to Semaphore with Python!'.format(name))
+cmdstr = dict(json.loads(sys.argv[1].replace("'", '"'))) 
+print(json.dumps(cmdstr))
